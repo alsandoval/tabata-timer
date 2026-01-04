435638dc63a65c64f05c76f0d3fced25579a2681
alsandoval <al.sandoval.5@gmail.com>
2026-01-03 16:48:24 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tabata-timer/tabata-timer/",
+  base: "/tabata-timer/",
 })
